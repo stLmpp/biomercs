8b7c442f64c81fddb4771cb0cf79d5fedf0f2ec9
stLmpp <gui.stlmpp@hotmail.com>
2021-06-10 21:56:47 -0300

    - fixed mobile and pc height (thanks to https://css-tricks.com/the-trick-to-viewport-units-on-mobile/)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { TitleService } from '@shared/title/title.service';
 import { BreakpointObserverService } from '@shared/services/breakpoint-observer/breakpoint-observer.service';
 import { MetaService } from '@shared/meta/meta.service';
+import { GlobalListenersService } from '@shared/services/global-listeners/global-listeners.service';
+import { Destroyable } from '@shared/components/common/destroyable-component';
+import { DOCUMENT } from '@angular/common';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'bio-root',
@@ -9,16 +13,33 @@ import { MetaService } from '@shared/meta/meta.service';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent extends Destroyable implements OnInit, OnDestroy {
   constructor(
     private titleService: TitleService,
     private breakpointObserverService: BreakpointObserverService,
-    private metaService: MetaService
-  ) {}
+    private metaService: MetaService,
+    private globalListenersService: GlobalListenersService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    super();
+  }
 
   isMobile$ = this.breakpointObserverService.isMobile$;
 
+  private _defineHeightPropertyCss(): void {
+    const vh = window.innerHeight * 0.01;
+    this.document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  private _listenToResize(): void {
+    this.globalListenersService.windowResize$.pipe(takeUntil(this.destroy$), debounceTime(500)).subscribe(() => {
+      this._defineHeightPropertyCss();
+    });
+  }
+
   ngOnInit(): void {
+    this._defineHeightPropertyCss();
+    this._listenToResize();
     this.titleService.init();
     this.metaService.init();
   }
@@ -26,5 +47,6 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.titleService.ngOnDestroy();
     this.metaService.ngOnDestroy();
+    super.ngOnDestroy();
   }
 }
