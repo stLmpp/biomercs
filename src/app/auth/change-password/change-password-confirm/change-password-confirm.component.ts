61af46497c9bdfa0e471f184e3b01f7ac0c59077
stLmpp <gui.stlmpp@hotmail.com>
2021-07-14 16:08:25 -0300

    - implemented method to confirm the password change - added text-danger to the error message

--- src/app/auth/change-password/change-password-confirm/change-password-confirm.component.ts
+++ src/app/auth/change-password/change-password-confirm/change-password-confirm.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Control, ControlGroup, Validators } from '@stlmpp/control';
 import { LocalState } from '@stlmpp/store';
-import { debounceTime } from 'rxjs';
+import { debounceTime, finalize, tap } from 'rxjs';
+import { AuthChangePassword } from '@model/auth';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouteParamEnum } from '@model/enum/route-param.enum';
+import { AuthService } from '../../auth.service';
+import { catchAndThrow } from '@util/operators/catch-and-throw';
+import { SnackBarService } from '@shared/components/snack-bar/snack-bar.service';
 
 interface AuthChangePasswordForm {
-  code: string | null;
+  code: number | null;
   oldPassword: string;
   newPassword: string;
   confirmNewPassword: string;
@@ -15,7 +21,16 @@ interface AuthChangePasswordComponentState {
   hidePassword: boolean;
   hideConfirmPassword: boolean;
   error: string | null;
+  confirming: boolean;
 }
+
+const initialState: AuthChangePasswordComponentState = {
+  hideOldPassword: true,
+  hideConfirmPassword: true,
+  hidePassword: true,
+  error: null,
+  confirming: false,
+};
 
 @Component({
   selector: 'bio-change-password-confirm',
@@ -25,12 +40,18 @@ interface AuthChangePasswordComponentState {
   host: { class: 'center-container' },
 })
 export class ChangePasswordConfirmComponent extends LocalState<AuthChangePasswordComponentState> {
-  constructor() {
-    super({ hideOldPassword: true, hideConfirmPassword: true, hidePassword: true, error: null });
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService,
+    private snackBarService: SnackBarService,
+    private router: Router
+  ) {
+    super(initialState);
   }
 
   state$ = this.selectState(['hideOldPassword', 'hideConfirmPassword', 'hidePassword']);
   error$ = this.selectState('error');
+  confirming$ = this.selectState('confirming');
 
   form = new ControlGroup<AuthChangePasswordForm>({
     code: new Control(null, [Validators.required]),
@@ -60,5 +81,39 @@ export class ChangePasswordConfirmComponent extends LocalState<AuthChangePasswor
 
   toggleHideConfirmPassword(): void {
     this.updateState('hideConfirmPassword', hideConfirmPassword => !hideConfirmPassword);
+  }
+
+  onConfirm(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    const { newPassword, oldPassword, code } = this.form.value;
+    // Safe to use, because there's a guard that validates this param
+    const key = this.activatedRoute.snapshot.paramMap.get(RouteParamEnum.key)!;
+    this.updateState({ confirming: true });
+    this.form.disable();
+    const dto: AuthChangePassword = {
+      newPassword,
+      oldPassword,
+      key,
+      // Also safe to use, because there's form validation
+      confirmationCode: code!,
+    };
+    this.authService
+      .confirmChangePassword(dto)
+      .pipe(
+        tap(() => {
+          this.updateState({ confirming: false, error: null });
+          this.snackBarService.open('Password changed successfully!');
+          this.router.navigate(['/']).then();
+        }),
+        catchAndThrow(error => {
+          this.updateState({ error: error.message, confirming: false });
+        }),
+        finalize(() => {
+          this.form.enable();
+        })
+      )
+      .subscribe();
   }
 }
