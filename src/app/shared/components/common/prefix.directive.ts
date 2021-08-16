cdfdeedf2836e1b3169ba61cbe46cfbe1b316f7f
stLmpp <gui.stlmpp@hotmail.com>
2021-08-16 17:28:56 -0300

    - include bio- in the selector of SuffixDirective and PrefixDirective

--- src/app/shared/components/common/prefix.directive.ts
+++ src/app/shared/components/common/prefix.directive.ts
@@ -2,7 +2,7 @@ import { Directive } from '@angular/core';
 import { FormFieldChild } from '@shared/components/form/form-field-child';
 
 @Directive({
-  selector: '[prefix]',
+  selector: '[bioPrefix]',
   host: { class: 'prefix', '[tabindex]': '-1' },
   providers: [{ provide: FormFieldChild, useExisting: PrefixDirective }],
 })
--- src/app/shared/components/common/suffix.directive.ts
+++ src/app/shared/components/common/suffix.directive.ts
@@ -2,7 +2,7 @@ import { Directive } from '@angular/core';
 import { FormFieldChild } from '@shared/components/form/form-field-child';
 
 @Directive({
-  selector: '[suffix]',
+  selector: '[bioSuffix]',
   host: { class: 'suffix', '[tabindex]': '-1' },
   providers: [{ provide: FormFieldChild, useExisting: SuffixDirective }],
 })
