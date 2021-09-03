47bfb63a473f979121316e8121ab1118d3fc14fb
stLmpp <gui.stlmpp@hotmail.com>
2021-09-03 08:37:25 -0300

    - fixup

--- src/app/shared/multi-select/multi-select.module.ts
+++ src/app/shared/multi-select/multi-select.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MultiSelectComponent } from './multi-select.component';
+import { MultiSelectItemsComponent } from './multi-select-items.component';
+import { MultiSelectItemDirective } from './multi-select-item.directive';
 import { FormModule } from '@shared/components/form/form.module';
 import { IconModule } from '@shared/components/icon/icon.module';
 import { ListModule } from '@shared/components/list/list.module';
 import { ButtonModule } from '@shared/components/button/button.module';
-import { MultiSelectComponent } from './multi-select.component';
-import { MultiSelectItemsComponent } from './multi-select-items.component';
-import { MultiSelectItemDirective } from './multi-select-item.directive';
 
 const DECLARATIONS = [MultiSelectComponent, MultiSelectItemsComponent, MultiSelectItemDirective];
-
 const MODULES = [FormModule, IconModule, ListModule, ButtonModule];
 
 @NgModule({
